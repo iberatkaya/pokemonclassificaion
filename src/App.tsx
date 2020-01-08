daa61c917cd30652f81ce4cb5d8fd6e12004248b
iberatkaya <beratkaya1998@gmail.com>
2020-01-08 04:55:17 +0300

    Added project

---
+++ src/App.tsx
@@ -0,0 +1,198 @@
+import React, { Component } from 'react';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import * as tf from '@tensorflow/tfjs';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import SquirtleData from './data/squirtle/squirtle';
+import CharmanderData from './data/charmander/charmander';
+import BulbasaurData from './data/bulbasaur/bulbasaur';
+import './App.css';
+
+/**
+ * A simple React App to test image recognition models MobileNet and COCOSSD
+ *
+ * @author iberatkaya
+ */
+
+interface Props {
+
+};
+
+/**
+ * State of the React App
+ * 
+ * @property {string} image The file path of the image
+ * @property {boolean} loading To check if the models have loaded
+ * @property {mobilenet.MobileNet} modelMobilenet The MobileNet model 
+ * @property {knnClassifier.KNNClassifier} classifier The KNN Classifier
+ * @property {Array<object>} mobilenetPred The predictions of the MobileNet model,
+ * @property {object} knnPred The predictions of the KNN model,
+ * @property {boolean} scanned To check if the image was scanned
+ */
+
+interface State {
+  image: string,
+  loading: boolean,
+  modelMobilenet: mobilenet.MobileNet | null,
+  mobilenetPred: Array<object>,
+  knnPred: {
+    label: string;
+    classIndex: number;
+    confidences: {
+      [label: string]: number;
+    };
+  } | null,
+  scanned: boolean,
+  classifier: knnClassifier.KNNClassifier | null
+}
+
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      image: '',
+      loading: true,
+      knnPred: null,
+      modelMobilenet: null,
+      mobilenetPred: [],
+      scanned: false,
+      classifier: null
+    }
+    let arr = [Object.values(BulbasaurData), Object.values(CharmanderData), Object.values(SquirtleData)].flat()
+    for (let i = 0; i < arr.length; i++) {
+      const im = new Image()
+      im.src = arr[i];
+      im.width = 50;
+      im.height = 50;
+      im.className = "hidden";
+      document.body.appendChild(im);
+      im.id = "img" + (i + 1).toString()
+    }
+  }
+
+  async componentDidMount() {
+    const modelMobilenet = await mobilenet.load({ version: 2, alpha: 1 });
+    const classifier = knnClassifier.create();
+    let arr = [Object.values(BulbasaurData), Object.values(CharmanderData), Object.values(SquirtleData)].flat()
+    for (let i = 0; i < arr.length; i++) {
+      let image = tf.browser.fromPixels(document.getElementById("img" + (i + 1).toString()) as HTMLImageElement);
+      const inferred = modelMobilenet.infer(image);
+      if (i < 100)
+        classifier.addExample(inferred, 'Balbasaur');
+      else if (i >= 100 && i < 199)
+        classifier.addExample(inferred, 'Charmander');
+      else
+        classifier.addExample(inferred, 'Squirtle');
+    }
+    this.setState({ modelMobilenet, classifier, loading: false }, () => console.log(this.state.classifier?.getNumClasses()));
+  }
+
+
+  onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    let file = URL.createObjectURL(e.target.files![0]);
+    this.setState({ image: file })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Navbar bg="success" variant="dark" expand="lg">
+          <Navbar.Brand style={{ color: '#eee' }} href="#home">Pokemon Classification</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link style={{ color: '#eee' }} target="_blank" rel="noopener noreferrer" href="https://github.com/iberatkaya">GitHub</Nav.Link>
+              <Nav.Link style={{ color: '#eee' }} target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ibrahim-berat-kaya">LinkedIn</Nav.Link>
+              <Nav.Link style={{ color: '#eee' }} target="_blank" rel="noopener noreferrer" href="https://www.npmjs.com/package/@tensorflow-models/mobilenet">MobileNet</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2 className="m-3">Pokemon Classification</h2>
+            </div>
+          </div>
+        </div>
+
+        {this.state.loading ?
+          <div className="container text-center">
+            <div className="spinner-border text-danger mb-3" role="status">
+            </div>
+            <div className="lead">Loading Models...</div>
+          </div>
+          :
+          <div>
+            {
+              this.state.image === '' ?
+                <div className="container justify-center align-items-center">
+                  <div className="text-center">
+                    <p className="lead">Upload your Pokemon image to classify it. Images are clasified with Tensorflow.js and MobileNet using transfer learning. Currently only Bulbasaur, Charmander, and Squirtle are classified.</p>
+                  </div>
+                  <form className="w-75 form">
+                    <div className="input-group">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">Upload</span>
+                      </div>
+                      <div className="custom-file">
+                        <input value={this.state.image} onChange={this.onChange} accept="image/*" type="file" className="custom-file-input" />
+                        <label className="custom-file-label">Choose file</label>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                :
+
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <div className="text-center">
+                        <img alt="input" ref="image" style={{ maxWidth: '60%' }} className="img-responsive" src={this.state.image}></img>
+                      </div>
+                      <p></p> {/* Moves button to bottom of image */}
+                      {!this.state.scanned ?
+                        <div className="text-center">
+                          <button className="btn btn-outline-primary" onClick={async () => {
+                            const modelMobilenet = this.state.modelMobilenet;
+                            const classifier = this.state.classifier;
+                            const predmobilenet = await modelMobilenet!.classify(this.refs.image as HTMLImageElement);
+                            const activation = modelMobilenet!.infer(this.refs.image as HTMLImageElement);
+                            const predclass = await classifier!.predictClass(activation);
+                            console.log(predmobilenet);
+                            console.log(predclass);
+                            this.setState({ mobilenetPred: predmobilenet, knnPred: predclass, scanned: true });
+
+                          }}>Classify</button>
+                        </div>
+                        :
+                        <div className="container">
+                          <ul className="list-group">
+                            <li className="list-group-item disabled">MobileNet</li>
+                            <li className="list-group-item disabled">KNN</li>
+                            <li className="list-group-item">Prediction: Balbasaur - Probability: {this.state.knnPred!.confidences.Balbasaur}</li>
+                            <li className="list-group-item">Prediction: Charmander - Probability: {this.state.knnPred!.confidences.Charmander}</li>
+                            <li className="list-group-item">Prediction: Squirtle - Probability: {this.state.knnPred!.confidences.Squirtle}</li>
+                          </ul>
+                          <div className="text-center">
+                            <button className="btn btn-outline-primary" onClick={async () => {
+                              this.setState({ image: '', mobilenetPred: [], scanned: false })
+                            }}>Classify New Image</button>
+                          </div>
+                        </div>
+                      }
+                    </div>
+                  </div>
+                </div>
+            }
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/data/bulbasaur/bulbasaur.ts
@@ -0,0 +1,205 @@
+import img0 from './00000000.jpg'
+import img1 from './00000001.jpg'
+import img2 from './00000002.jpg'
+import img3 from './00000003.jpg'
+import img4 from './00000004.jpg'
+import img5 from './00000005.jpg'
+import img6 from './00000006.jpg'
+import img7 from './00000007.jpg'
+import img8 from './00000008.jpg'
+import img9 from './00000009.jpg'
+import img10 from './00000010.jpg'
+import img11 from './00000011.jpg'
+import img12 from './00000012.jpg'
+import img13 from './00000013.jpg'
+import img14 from './00000014.jpg'
+import img15 from './00000015.jpg'
+import img16 from './00000016.jpg'
+import img17 from './00000017.jpg'
+import img18 from './00000018.jpg'
+import img19 from './00000019.jpg'
+import img20 from './00000020.jpg'
+import img21 from './00000021.jpg'
+import img22 from './00000022.jpg'
+import img23 from './00000023.jpg'
+import img24 from './00000024.jpg'
+import img25 from './00000025.jpg'
+import img26 from './00000026.jpg'
+import img27 from './00000027.jpg'
+import img28 from './00000028.jpg'
+import img29 from './00000029.jpg'
+import img30 from './00000030.jpg'
+import img31 from './00000031.jpg'
+import img32 from './00000032.jpg'
+import img33 from './00000033.jpg'
+import img34 from './00000034.jpg'
+import img35 from './00000035.jpg'
+import img36 from './00000036.jpg'
+import img37 from './00000037.jpg'
+import img38 from './00000038.jpg'
+import img39 from './00000039.jpg'
+import img40 from './00000040.jpg'
+import img41 from './00000041.jpg'
+import img42 from './00000042.jpg'
+import img43 from './00000043.jpg'
+import img44 from './00000044.jpg'
+import img45 from './00000045.jpg'
+import img46 from './00000046.jpg'
+import img47 from './00000047.jpg'
+import img48 from './00000048.jpg'
+import img49 from './00000049.jpg'
+import img50 from './00000050.jpg'
+import img51 from './00000051.jpg'
+import img52 from './00000052.jpg'
+import img53 from './00000053.jpg'
+import img54 from './00000054.jpg'
+import img55 from './00000055.jpg'
+import img56 from './00000056.jpg'
+import img57 from './00000057.jpg'
+import img58 from './00000058.jpg'
+import img59 from './00000059.jpg'
+import img60 from './00000060.jpg'
+import img61 from './00000061.jpg'
+import img62 from './00000062.jpg'
+import img63 from './00000063.jpg'
+import img64 from './00000064.jpg'
+import img65 from './00000065.jpg'
+import img66 from './00000066.jpg'
+import img67 from './00000067.jpg'
+import img68 from './00000068.jpg'
+import img69 from './00000069.jpg'
+import img70 from './00000070.jpg'
+import img71 from './00000071.jpg'
+import img72 from './00000072.jpg'
+import img73 from './00000073.jpg'
+import img74 from './00000074.jpg'
+import img75 from './00000075.jpg'
+import img76 from './00000076.jpg'
+import img77 from './00000077.jpg'
+import img78 from './00000078.jpg'
+import img79 from './00000079.jpg'
+import img80 from './00000080.jpg'
+import img81 from './00000081.jpg'
+import img82 from './00000082.jpg'
+import img83 from './00000083.jpg'
+import img84 from './00000084.jpg'
+import img85 from './00000085.jpg'
+import img86 from './00000086.jpg'
+import img87 from './00000087.jpg'
+import img88 from './00000088.jpg'
+import img89 from './00000089.jpg'
+import img90 from './00000090.jpg'
+import img91 from './00000091.jpg'
+import img92 from './00000092.jpg'
+import img93 from './00000093.jpg'
+import img94 from './00000094.jpg'
+import img95 from './00000095.jpg'
+import img96 from './00000096.jpg'
+import img97 from './00000097.jpg'
+import img98 from './00000098.jpg'
+import img99 from './00000099.jpg'
+
+let obj = {
+   img0: img0,
+   img1: img1,
+   img2: img2,
+   img3: img3,
+   img4: img4,
+   img5: img5,
+   img6: img6,
+   img7: img7,
+   img8: img8,
+   img9: img9,
+   img10: img10,
+   img11: img11,
+   img12: img12,
+   img13: img13,
+   img14: img14,
+   img15: img15,
+   img16: img16,
+   img17: img17,
+   img18: img18,
+   img19: img19,
+   img20: img20,
+   img21: img21,
+   img22: img22,
+   img23: img23,
+   img24: img24,
+   img25: img25,
+   img26: img26,
+   img27: img27,
+   img28: img28,
+   img29: img29,
+   img30: img30,
+   img31: img31,
+   img32: img32,
+   img33: img33,
+   img34: img34,
+   img35: img35,
+   img36: img36,
+   img37: img37,
+   img38: img38,
+   img39: img39,
+   img40: img40,
+   img41: img41,
+   img42: img42,
+   img43: img43,
+   img44: img44,
+   img45: img45,
+   img46: img46,
+   img47: img47,
+   img48: img48,
+   img49: img49,
+   img50: img50,
+   img51: img51,
+   img52: img52,
+   img53: img53,
+   img54: img54,
+   img55: img55,
+   img56: img56,
+   img57: img57,
+   img58: img58,
+   img59: img59,
+   img60: img60,
+   img61: img61,
+   img62: img62,
+   img63: img63,
+   img64: img64,
+   img65: img65,
+   img66: img66,
+   img67: img67,
+   img68: img68,
+   img69: img69,
+   img70: img70,
+   img71: img71,
+   img72: img72,
+   img73: img73,
+   img74: img74,
+   img75: img75,
+   img76: img76,
+   img77: img77,
+   img78: img78,
+   img79: img79,
+   img80: img80,
+   img81: img81,
+   img82: img82,
+   img83: img83,
+   img84: img84,
+   img85: img85,
+   img86: img86,
+   img87: img87,
+   img88: img88,
+   img89: img89,
+   img90: img90,
+   img91: img91,
+   img92: img92,
+   img93: img93,
+   img94: img94,
+   img95: img95,
+   img96: img96,
+   img97: img97,
+   img98: img98,
+   img99: img99,
+};
+
+export default obj;
